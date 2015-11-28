fb6acd08ada95f37a96ec4b5ceeef15ddc6e5185
nmsbane <madhusudhan638@gmail.com>
2015-11-28 14:37:33 +0530

    now the screenshots are handled by phantomjs instead of gribzit api

--- app/main/views.py
+++ app/main/views.py
@@ -1,27 +1,13 @@
 import uuid
 import boto
-from flask import redirect, url_for, render_template, current_app, flash
+from flask import redirect, url_for, render_template, flash
 from flask.ext.login import login_required, current_user
 from . import main
 from .. import db
-from manage import app
 from .forms import Bookmarks
 from ..models import BookMarks
-from GrabzIt import GrabzItClient
-from boto.s3.connection import S3Connection
-from boto.s3.key import Key
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-with app.app_context():
-    grabzIt = GrabzItClient.GrabzItClient(current_app.config['APPLICATION_KEY'],
-                                          current_app.config['APPLICATION_SECRET'])
-    conn = S3Connection(current_app.config['AWS_ACCESS_KEY_ID'],
-                        current_app.config['AWS_SECRET_ACCESS_KEY']
-                        )
-    bucket_name = 'banesbookmarks'
-    bucket = conn.get_bucket(bucket_name)
-    k = Key(bucket)
 
 
 @main.route('/')
@@ -34,25 +20,6 @@ def index():
 def bookmarks():
     bookmark_urls = current_user.bookmarks.all()
     return render_template('index.html', bookmarks=bookmark_urls)
-
-
-def save_to_s3(url):
-    grabzIt.SetImageOptions(url, width="200", height="200")
-    hashvalue = uuid.uuid4()
-    filepath = '%s.jpg' % str(hashvalue)
-    k.key = filepath
-    grabzIt.SaveTo(filepath)
-    k.set_contents_from_string(open(filepath, 'r').read())
-    os.remove(filepath)
-    return str(hashvalue)
-
-
-def save_to_local(url):
-    grabzIt.SetImageOptions(url)
-    hashvalue = uuid.uuid4()
-    filepath = basedir + '/../static/images/%s.jpg' % str(hashvalue)
-    grabzIt.SaveTo(filepath)
-    return hashvalue
 
 
 @main.route('/addbookmarks', methods=['GET', 'POST'])
@@ -72,12 +39,10 @@ def add_bookmarks():
             if bookmark.url == url:
                 flash('The URL u are about to add already exists')
                 return redirect(url_for('main.bookmarks'))
-        filename = save_to_s3(url)
-        #filename = save_to_local(url)
         bookmark_object = BookMarks()
         bookmark_object.description = form.comment.data
-        bookmark_object.url = url
-        bookmark_object.image_url = str('https://s3-us-west-2.amazonaws.com/banesbookmarks/%s' % (filename+'.jpg'))
+        if not bookmark_object.fetch_image(url):
+            render_template('error.html')
         bookmark_object.user = active_user
         db.session.add(bookmark_object)
         return redirect(url_for('main.bookmarks'))
